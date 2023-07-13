9b7360ad53bae641dbbfaefb8ce8f500ab6310fa
Sayan Naskar <nascarsayan@gmail.com>
2023-07-13 09:14:58 +0530

    add delay pools support

--- squid-proxy-setup/.docker/build-push.sh
+++ squid-proxy-setup/.docker/build-push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-SQUID_IMG="${SQUID_IMG:-"nascarsayan/squid-proxy:latest"}"
+set -x
+SQUID_IMG="${SQUID_IMG:-"nascarsayan/squid-proxy"}"
 TAG="${1:-"latest"}"
 sudo docker build --no-cache . -t "${SQUID_IMG}:${TAG}"
 sudo docker push "${SQUID_IMG}:${TAG}"
+set +x
